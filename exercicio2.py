4e4a550c0eeb13794d15048ace3db3c9fb865539
murillomsouza <murillosouza997@gmail.com>
2025-02-26 09:56:39 -0300

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio2"""
-nt1 = float(input('Digite a primeira nota do aluno: '))
-nt2 = float(input('Digite a segunda nota do aluno: '))
-media = (nt1+nt2)/2
-print(f'A media desse aluno é {media}')
