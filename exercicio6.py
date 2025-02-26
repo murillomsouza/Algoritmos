27fc901465dec718b13dd89d00a7bca717c91488
murillomsouza <murillosouza997@gmail.com>
2025-02-26 09:57:28 -0300

    Delete exercicio6.py

--- exercicio6.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio6"""
-base = float(input('Digite a base do triangulo:'))
-altura = float(input('Digite e altura do triangulo:'))
-areatri = (base*altura)/2
-print(f'A area do triangulo é {areatri}')
